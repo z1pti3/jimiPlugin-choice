85af5610d74d3ebf918b976c0c4c7184dabb5b45
z1pti3 <z1pti3@outlook.com>
2021-02-23 19:08:54 +0000

    Added manifest

--- choice.py
+++ choice.py
@@ -5,7 +5,7 @@ from core.models import conduct, trigger, webui
 from plugins.choice.models import action
 
 class _choice(plugin._plugin):
-    version = 0.2
+    version = 1.0
 
     def install(self):
         # Register models
